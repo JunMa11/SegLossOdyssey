208dea3a268ffca3c78e4c04b56fbefc43770ff7
Jun <1259389904@qq.com>
2019-08-06 08:53:00 -0400

    Update ND_Crossentropy.py

--- loss_functions/ND_Crossentropy.py
+++ loss_functions/ND_Crossentropy.py
@@ -37,3 +37,32 @@ class CrossentropyND(torch.nn.CrossEntropyLoss):
         target = target.view(-1,)
 
         return super(CrossentropyND, self).forward(inp, target)
+
+class WeightedCrossEntropyLoss(torch.nn.CrossEntropyLoss):
+    """
+    Network has to have NO NONLINEARITY!
+    """
+    def __init__(self, weight=None):
+        super(WeightedCrossEntropyLoss, self).__init__()
+        # predefined weight, shape = (class_num)
+        self.weight = weight
+
+    def forward(self, inp, target):
+        target = target.long()
+        num_classes = inp.size()[1]
+
+        i0 = 1
+        i1 = 2
+
+        while i1 < len(inp.shape): # this is ugly but torch only allows to transpose two axes at once
+            inp = inp.transpose(i0, i1)
+            i0 += 1
+            i1 += 1
+
+        inp = inp.contiguous()
+        inp = inp.view(-1, num_classes)
+
+        target = target.view(-1,)
+        wce_loss = torch.nn.CrossEntropyLoss(weight=self.weight)
+
+        return wce_loss(inp, target)
