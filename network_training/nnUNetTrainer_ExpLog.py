1b2d9a17b9354cce7d983cd184c2b4629c2c6264
Jun <1259389904@qq.com>
2019-08-06 16:33:14 -0400

    Add files via upload

--- network_training/nnUNetTrainer_ExpLog.py
+++ network_training/nnUNetTrainer_ExpLog.py
@@ -1,5 +1,6 @@
 from nnunet.training.loss_functions.dice_loss import ExpLog_loss
 from nnunet.training.network_training.nnUNetTrainer import nnUNetTrainer
+import torch
 
 
 class nnUNetTrainer_ExpLog(nnUNetTrainer):
@@ -7,5 +8,5 @@ class nnUNetTrainer_ExpLog(nnUNetTrainer):
                  unpack_data=True, deterministic=True, fp16=False):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
                                               unpack_data, deterministic, fp16)
-
-        self.loss = ExpLog_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False, 'square': False}, {})
+        self.weight = torch.cuda.FloatTensor([0.2,0.8])
+        self.loss = ExpLog_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False, 'square': False}, {'weight':self.weight})
