aa6b06fdf9371725bc4fe35ae9fc8cdf88398668
Jun <1259389904@qq.com>
2020-12-14 16:32:31 +0800

    Add files via upload

--- test/loss_functions/boundary_loss.py
+++ test/loss_functions/boundary_loss.py
@@ -5,6 +5,7 @@ from nnunet.training.loss_functions.ND_Crossentropy import CrossentropyND
 from nnunet.utilities.tensor_utilities import sum_tensor
 from torch import nn
 from scipy.ndimage import distance_transform_edt
+from skimage import segmentation as skimage_seg
 import numpy as np
 
 def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
@@ -60,33 +61,6 @@ def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
     return tp, fp, fn
 
 
-class BDLoss(nn.Module):
-    def __init__(self):
-        """
-        compute boudary loss
-        only compute the loss of foreground
-        ref: https://github.com/LIVIAETS/surface-loss/blob/108bd9892adca476e6cdf424124bc6268707498e/losses.py#L74
-        """
-        super(BDLoss, self).__init__()
-        # self.do_bg = do_bg
-
-    def forward(self, net_output, target, bound):
-        """
-        net_output: (batch_size, class, x,y,z)
-        target: ground truth, shape: (batch_size, 1, x,y,z)
-        bound: precomputed distance map, shape (batch_size, class, x,y,z)
-        """
-        net_output = softmax_helper(net_output)
-        # print('net_output shape: ', net_output.shape)
-        pc = net_output[:, 1:, ...].type(torch.float32)
-        dc = bound[:,1:, ...].type(torch.float32)
-
-        multipled = torch.einsum("bcxyz,bcxyz->bcxyz", pc, dc)
-        bd_loss = multipled.mean()
-
-        return bd_loss
-
-
 class SoftDiceLoss(nn.Module):
     def __init__(self, apply_nonlin=None, batch_dice=False, do_bg=True, smooth=1.,
                  square=False):
@@ -123,118 +97,155 @@ class SoftDiceLoss(nn.Module):
                 dc = dc[:, 1:]
         dc = dc.mean()
 
-        return -dc
+        return 1-dc
+
+def compute_sdf(img_gt, out_shape):
+    """
+    compute the signed distance map of binary mask
+    input: segmentation, shape = (batch_size, x, y, z)
+    output: the Signed Distance Map (SDM) 
+    sdf(x) = 0; x in segmentation boundary
+             -inf|x-y|; x in segmentation
+             +inf|x-y|; x out of segmentation
+    """
+
+    img_gt = img_gt.astype(np.uint8)
+
+    gt_sdf = np.zeros(out_shape)
+
+    for b in range(out_shape[0]): # batch size
+        for c in range(1, out_shape[1]): # channel
+            posmask = img_gt[b][c].astype(np.bool)
+            if posmask.any():
+                negmask = ~posmask
+                posdis = distance_transform_edt(posmask)
+                negdis = distance_transform_edt(negmask)
+                boundary = skimage_seg.find_boundaries(posmask, mode='inner').astype(np.uint8)
+                sdf = negdis - posdis
+                sdf[boundary==1] = 0
+                gt_sdf[b][c] = sdf
+
+    return gt_sdf
+
+
+class BDLoss(nn.Module):
+    def __init__(self):
+        """
+        compute boudary loss
+        only compute the loss of foreground
+        ref: https://github.com/LIVIAETS/surface-loss/blob/108bd9892adca476e6cdf424124bc6268707498e/losses.py#L74
+        """
+        super(BDLoss, self).__init__()
+        # self.do_bg = do_bg
+
+    def forward(self, net_output, gt):
+        """
+        net_output: (batch_size, class, x,y,z)
+        target: ground truth, shape: (batch_size, 1, x,y,z)
+        bound: precomputed distance map, shape (batch_size, class, x,y,z)
+        """
+        net_output = softmax_helper(net_output)
+        with torch.no_grad():
+            if len(net_output.shape) != len(gt.shape):
+                gt = gt.view((gt.shape[0], 1, *gt.shape[1:]))
+
+            if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+                # if this is the case then gt is probably already a one hot encoding
+                y_onehot = gt
+            else:
+                gt = gt.long()
+                y_onehot = torch.zeros(net_output.shape)
+                if net_output.device.type == "cuda":
+                    y_onehot = y_onehot.cuda(net_output.device.index)
+                y_onehot.scatter_(1, gt, 1)
+            gt_sdf = compute_sdf(y_onehot.cpu().numpy(), net_output.shape)
+
+        phi = torch.from_numpy(gt_sdf)
+        if phi.device != net_output.device:
+            phi = phi.to(net_output.device).type(torch.float32)
+        # pred = net_output[:, 1:, ...].type(torch.float32)
+        # phi = phi[:,1:, ...].type(torch.float32)
+
+        multipled = torch.einsum("bcxyz,bcxyz->bcxyz", net_output[:, 1:, ...], phi[:, 1:, ...])
+        bd_loss = multipled.mean()
+
+        return bd_loss
+
+
 
 class DC_and_BD_loss(nn.Module):
     def __init__(self, soft_dice_kwargs, bd_kwargs, aggregate="sum"):
         super(DC_and_BD_loss, self).__init__()
         self.aggregate = aggregate
-        self.bd = BDLoss(**bd_kwargs)
         self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+        self.bd = BDLoss(**bd_kwargs)
+        
 
-    def forward(self, net_output, target, bound):
+    def forward(self, net_output, target):
         dc_loss = self.dc(net_output, target)
-        bd_loss = self.bd(net_output, target, bound)
+        bd_loss = self.bd(net_output, target)
         if self.aggregate == "sum":
-            result = dc_loss + bd_loss
+            result = 0.01*dc_loss + (1-0.01)*bd_loss
         else:
-            raise NotImplementedError("nah son") # reserved for other stuff (later)
+            raise NotImplementedError("nah son") 
         return result       
 
 
-def compute_edts_forhdloss(segmentation):
-    res = np.zeros(segmentation.shape)
-    for i in range(segmentation.shape[0]):
-        posmask = segmentation[i]
-        negmask = ~posmask
-        res[i] = distance_transform_edt(posmask) + distance_transform_edt(negmask)
-    return res
+#####################################################
 
+def compute_gt_dtm(img_gt, out_shape):
+    """
+    compute the distance transform map of foreground in ground gruth.
+    input: segmentation, shape = (batch_size, class, x, y, z)
+    output: the foreground Distance Map (SDM) 
+    dtm(x) = 0; x in segmentation boundary
+             inf|x-y|; x in segmentation
+    """
 
-class HDDTBinaryLoss(nn.Module):
+    fg_dtm = np.zeros(out_shape)
+
+    for b in range(out_shape[0]): # batch size
+        for c in range(1, out_shape[1]): # class; exclude the background class
+            posmask = img_gt[b][c].astype(np.bool)
+            if posmask.any():
+                posdis = distance_transform_edt(posmask)
+                fg_dtm[b][c] = posdis
+
+    return fg_dtm
+
+def compute_pred_dtm(img_gt, out_shape):
+    """
+    compute the distance transform map of foreground in prediction.
+    input: segmentation, shape = (batch_size, class, x, y, z)
+    output: the foreground Distance Map (SDM) 
+    dtm(x) = 0; x in segmentation boundary
+             inf|x-y|; x in segmentation
+    """
+
+    fg_dtm = np.zeros(out_shape)
+
+    for b in range(out_shape[0]): # batch size
+        for c in range(1, out_shape[1]): # class; exclude the background class
+            posmask = img_gt[b][c]>0.5
+            if posmask.any():
+                posdis = distance_transform_edt(posmask)
+                fg_dtm[b][c] = posdis
+
+    return fg_dtm
+
+class HDLoss(nn.Module):
     def __init__(self):
         """
         compute haudorff loss for binary segmentation
         https://arxiv.org/pdf/1904.10030v1.pdf        
         """
-        super(HDDTBinaryLoss, self).__init__()
+        super(HDLoss, self).__init__()
 
-
-    def forward(self, net_output, target):
-        """
-        net_output: (batch_size, 2, x,y,z)
-        target: ground truth, shape: (batch_size, 1, x,y,z)
-        """
-        net_output = softmax_helper(net_output)
-        pc = net_output[:, 1, ...].type(torch.float32)
-        gt = target[:,0, ...].type(torch.float32)
-        with torch.no_grad():
-            pc_dist = compute_edts_forhdloss(pc.cpu().numpy()>0.5)
-            gt_dist = compute_edts_forhdloss(gt.cpu().numpy()>0.5)
-        # print('pc_dist.shape: ', pc_dist.shape)
-        
-        pred_error = (gt - pc)**2
-        dist = pc_dist**2 + gt_dist**2 # \alpha=2 in eq(8)
-
-        dist = torch.from_numpy(dist)
-        if dist.device != pred_error.device:
-            dist = dist.to(pred_error.device).type(torch.float32)
-
-        multipled = torch.einsum("bxyz,bxyz->bxyz", pred_error, dist)
-        hd_loss = multipled.mean()
-
-        return hd_loss
-
-
-
-class DC_and_HDBinary_loss(nn.Module):
-    def __init__(self, soft_dice_kwargs, hd_kwargs, aggregate="sum"):
-        super(DC_and_HDBinary_loss, self).__init__()
-        self.aggregate = aggregate
-        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
-        self.hd = HDDTBinaryLoss(**hd_kwargs)
-
-    def forward(self, net_output, target):
-        dc_loss = self.dc(net_output, target)
-        hd_loss = self.hd(net_output, target)
-        if self.aggregate == "sum":
-            result = dc_loss + hd_loss
-        else:
-            raise NotImplementedError("nah son")
-        return result    
-
-
-def compute_edts_forPenalizedLoss(GT):
-    """
-    GT.shape = (batch_size, x,y,z)
-    only for binary segmentation
-    """
-    res = np.zeros(GT.shape)
-    for i in range(GT.shape[0]):
-        posmask = GT[i]
-        negmask = ~posmask
-        pos_edt = distance_transform_edt(posmask)
-        pos_edt = (np.max(pos_edt)-pos_edt)*posmask 
-        neg_edt =  distance_transform_edt(negmask)
-        neg_edt = (np.max(neg_edt)-neg_edt)*negmask
-        
-        res[i] = pos_edt/np.max(pos_edt) + neg_edt/np.max(neg_edt)
-    return res
-
-class DistBinaryDiceLoss(nn.Module):
-    """
-    Distance map penalized Dice loss
-    Motivated by: https://openreview.net/forum?id=B1eIcvS45V
-    Distance Map Loss Penalty Term for Semantic Segmentation        
-    """
-    def __init__(self, smooth=1e-5):
-        super(DistBinaryDiceLoss, self).__init__()
-        self.smooth = smooth
 
     def forward(self, net_output, gt):
         """
-        net_output: (batch_size, 2, x,y,z)
-        target: ground truth, shape: (batch_size, 1, x,y,z)
+        net_output: (batch_size, c, x,y,z)
+        target: ground truth, shape: (batch_size, c, x,y,z)
         """
         net_output = softmax_helper(net_output)
         # one hot code for gt
@@ -251,23 +262,43 @@ class DistBinaryDiceLoss(nn.Module):
                 if net_output.device.type == "cuda":
                     y_onehot = y_onehot.cuda(net_output.device.index)
                 y_onehot.scatter_(1, gt, 1)
-        
-        gt_temp = gt[:,0, ...].type(torch.float32)
+        # print('hd loss.py', net_output.shape, y_onehot.shape)
+
         with torch.no_grad():
-            dist = compute_edts_forPenalizedLoss(gt_temp.cpu().numpy()>0.5) + 1.0
-        # print('dist.shape: ', dist.shape)
+            pc_dist = compute_pred_dtm(net_output.cpu().numpy(), net_output.shape)
+            gt_dist = compute_gt_dtm(y_onehot.cpu().numpy(), net_output.shape)
+            dist = pc_dist**2 + gt_dist**2 # \alpha=2 in eq(8)
+            # print('pc_dist.shape: ', pc_dist.shape, 'gt_dist.shape', gt_dist.shape)
+        
+        pred_error = (net_output - y_onehot)**2
+
         dist = torch.from_numpy(dist)
+        if dist.device != pred_error.device:
+            dist = dist.to(pred_error.device).type(torch.float32)
 
-        if dist.device != net_output.device:
-            dist = dist.to(net_output.device).type(torch.float32)
-        
-        tp = net_output * y_onehot
-        tp = torch.sum(tp[:,1,...] * dist, (1,2,3))
-        
-        dc = (2 * tp + self.smooth) / (torch.sum(net_output[:,1,...], (1,2,3)) + torch.sum(y_onehot[:,1,...], (1,2,3)) + self.smooth)
+        multipled = torch.einsum("bcxyz,bcxyz->bcxyz", pred_error[:,1:,...], dist[:,1:,...])
+        hd_loss = multipled.mean()
 
-        dc = dc.mean()
+        return hd_loss
 
-        return -dc
+
+
+class DC_and_HD_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, hd_kwargs, aggregate="sum"):
+        super(DC_and_HD_loss, self).__init__()
+        self.aggregate = aggregate
+        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+        self.hd = HDLoss(**hd_kwargs)
+
+    def forward(self, net_output, target):
+        dc_loss = self.dc(net_output, target)
+        hd_loss = self.hd(net_output, target)
+        if self.aggregate == "sum":
+            with torch.no_grad():
+                alpha = hd_loss / (dc_loss + 1e-5 )
+            result = alpha * dc_loss + hd_loss
+        else:
+            raise NotImplementedError("nah son")
+        return result  
 
 
