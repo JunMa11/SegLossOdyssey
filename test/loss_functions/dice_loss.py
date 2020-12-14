bea91a4e72c78fe63335fe9333d6659489ceb1b4
Jun <1259389904@qq.com>
2020-12-14 16:43:18 +0800

    Update dice_loss.py

--- test/loss_functions/dice_loss.py
+++ test/loss_functions/dice_loss.py
@@ -15,6 +15,7 @@ import torch
 from nnunet.training.loss_functions.TopK_loss import TopKLoss
 from nnunet.utilities.nd_softmax import softmax_helper
 from nnunet.training.loss_functions.ND_Crossentropy import CrossentropyND, WeightedCrossEntropyLoss
+from nnunet.training.loss_functions.focal_loss import FocalLoss
 from nnunet.utilities.tensor_utilities import sum_tensor
 from torch import nn
 from torch.autograd import Variable
@@ -495,6 +496,17 @@ class ExpLog_loss(nn.Module):
 
         return explog_loss
 
+class DC_and_Focal_loss(nn.Module):
+    def __init__(self, soft_dice_kwargs, focal_kwargs):
+        super(DC_and_Focal_loss, self).__init__()
+        self.dc = SoftDiceLoss(apply_nonlin=softmax_helper, **soft_dice_kwargs)
+        self.focal = FocalLoss(apply_nonlin=softmax_helper, **focal_kwargs)
 
+    def forward(self, net_output, target):
+        dc_loss = self.dc(net_output, target)
+        focal_loss = self.focal(net_output, target)
+
+        result = dc_loss + focal_loss
+        return result
 
 
