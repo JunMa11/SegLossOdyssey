8838616323b6ffa7fdb522c7d0a5250be4610221
Jun <1259389904@qq.com>
2021-03-31 08:44:14 +0800

    Add files via upload

---
+++ test/network_training/nnUNetTrainer_DiceBD.py
@@ -0,0 +1,10 @@
+from nnunet.training.loss_functions.boundary_loss import DC_and_BD_loss
+from nnunet.training.network_training.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainer_DiceBD(nnUNetTrainer):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
+                                              unpack_data, deterministic, fp16)
+        self.loss = DC_and_BD_loss({'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False, 'square': False},{})
